72c401bab1b4c06168dae5490c95bb85ad7d5d2d
pardon6 <pardon6@github.com>
2018-03-16 09:37:13 +0900

    DB 백업_20180316

--- 데이터 정확성 확인.sql
+++ 데이터 정확성 확인.sql
@@ -1,5 +1,5 @@
-SELECT a.item_cd AS 아이템코드, b.nm AS 아이템명, count(*) AS 개수, min(date) AS 시작일, max(date) AS 마지막일
-  FROM value as a
-     , item as b
- WHERE a.item_cd = b.cd
-GROUP BY a.item_cd, b.nm
+SELECT a.cd AS 아이템코드, a.nm AS 아이템명, count(*) AS 개수, min(date) AS 시작일, max(date) AS 마지막일
+  FROM item as a     
+	LEFT JOIN value AS b
+    ON a.cd = b.item_cd
+GROUP BY a.cd, a.nm
